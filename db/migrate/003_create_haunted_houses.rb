398269d26e0a6bf227971d35c23194fcb3715985
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-21 14:31:32 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -11,7 +11,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       a.string :name
       a.string :location
       a.string :theme
-      a.string :family_friendly
+      a.integer :price
+      a.boolean :family_friendly
       a.datetime :opening_date
       a.datetime :closing_date
       a.string :description
